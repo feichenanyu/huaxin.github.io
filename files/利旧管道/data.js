c572ec6895867e765d1e98483d621e2ba3495ece
pollyzhu <ppfeichen@163.com>
2018-05-04 15:48:24 +0800

    new
    新建

---
+++ files/利旧管道/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(S,_(T,U,V,[_(T,W,X,g,Y,[_(Z,ba,T,bb,bc,_(bd,l,b,be,bf,bg),bh,bi)])])),bj,_(bk,[])),bl,_(),bm,_());}; 
+var b="url",c="利旧管道.html",d="generationDate",e=new Date(1525419821504.96),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="point",l="page",m="packageId",n="c481237b7fbb4f238cffd0c85c728c38",o="type",p="Axure:Page",q="name",r="利旧管道",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="onLoad",T="description",U="页面载入时",V="cases",W="Case 1",X="isNewIfGroup",Y="actions",Z="action",ba="linkWindow",bb="在 当前窗口 打开 【new】设置截面",bc="target",bd="targetType",be="【new】设置截面.html",bf="includeVariables",bg=true,bh="linkType",bi="current",bj="diagram",bk="objects",bl="masters",bm="objectPaths";
+return _creator();
+})());
